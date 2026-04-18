60b4e9e752505c376b1223ee23dc1d72fda05f1c
Yvon COCKS <ycocks@hn-services.fr>
2026-04-18 11:39:47 +0200

    Enum SelectList Text written with spaces

--- Models/Helpers/ConvertEnumToItem.cs
+++ Models/Helpers/ConvertEnumToItem.cs
@@ -13,12 +13,12 @@ namespace TPLOCAL1.Models.Helpers
         public static SelectList GetGenderSelectList()
         {
             var genderConvertEnum = new List<ConvertEnumToItem>();
-            foreach (Gender gender in Enum.GetValues(typeof(Gender))) 
+            foreach (Gender gender in Enum.GetValues(typeof(Gender)))
             {
                 genderConvertEnum.Add(new ConvertEnumToItem
                 {
                     Value = (int)gender,
-                    Text = gender.ToString()
+                    Text = AddSpacesToString(gender.ToString())
                 });
             }
 
@@ -29,15 +29,34 @@ namespace TPLOCAL1.Models.Helpers
         {
             var trainingTypeConvertEnum = new List<ConvertEnumToItem>();
             foreach (TrainingType trainingType in Enum.GetValues(typeof(TrainingType)))
-            { 
+            {
                 trainingTypeConvertEnum.Add(new ConvertEnumToItem
                 {
                     Value = (int)trainingType,
-                    Text = trainingType.ToString()
+                    Text = AddSpacesToString(trainingType.ToString())
                 });
             }
 
             return new SelectList(trainingTypeConvertEnum, "Value", "Text");
+        }
+
+        // from https://stackoverflow.com/questions/272633/add-spaces-before-capital-letters
+        private static string AddSpacesToString(string text, bool preserveAcronyms = false)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+            StringBuilder newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                    if ((text[i - 1] != ' ' && !char.IsUpper(text[i - 1])) ||
+                        (preserveAcronyms && char.IsUpper(text[i - 1]) &&
+                         i < text.Length - 1 && !char.IsUpper(text[i + 1])))
+                        newText.Append(' ');
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
         }
     }
 }
